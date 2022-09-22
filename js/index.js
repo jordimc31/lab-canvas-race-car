1eedd10707db0cc465d5bb7242243efe036fef1d
Jordi Martinez <jordiupc87@gmail.com>
2022-09-22 16:17:32 +0200

    Iteration 3 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,49 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    console.log("Starting game..");
+
+    // ITERATION 1 -- Draw Road
+
+    let canvaWindow = document.getElementById("canvas").getContext("2d");
+    let imgRoad = new Image();
+    imgRoad.src = "images/road.png";
+    imgRoad.onload = function () {
+      canvaWindow.drawImage(imgRoad, 0, 0);
+    };
+    // ITERATION 2 -- Draw Car
+
+    let imgCar = new Image();
+    imgCar.src = "images/car.png";
+
+    imgCar.onload = function () {
+      let carWidth = 25;
+      let carHeight = carWidth * 2;
+      canvaWindow.drawImage(
+        imgCar,
+        imgRoad.width / 2 - carWidth / 2,
+        imgRoad.height - carHeight,
+        carWidth,
+        carHeight
+      );
+
+      let randInt = Math.floor(Math.random() * (5000 - 1000) + 1000);
+    };
+
+    // ITERATION 3 -- Car Movement
+
+    window.addEventListener("keydown", (key) => {
+      if (key.key == "ArrowRight") {
+        console.log("ArrowRight");
+      }
+      if (key.key == "ArrowLeft") {
+        console.log("ArrowLeft");
+      }
+    });
+
+    //ITERATION 4 -- OBSTACLES
+  } // startGame end
 };
